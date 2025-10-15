7524de2f4bc839bcc410a14981d3dc5556fba048
Daniil Pototskiy <daniilpototskiy@mail.ru>
2025-10-16 00:24:29 +0300

    Added field on page for jwt token

---
+++ src/main/java/com/example/bankcards/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.example.bankcards.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bank Card Management API",
+                version = "1.0.0",
+                description = "API for creating, managing, and performing transactions between bank cards."
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
